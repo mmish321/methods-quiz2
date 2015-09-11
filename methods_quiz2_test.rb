62c28467e102107f0d3a9973dcc12ae89c41867a
Michelle Mao <maom@s.dcsdk12.org>
2015-09-11 09:53:52 -0600

    Tests for without doubles

--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -11,6 +11,12 @@ class MethodsQuizTest < MiniTest::Test
    	end.new
 	end
 
-	# TODO - write tests here
+	def test_without_doubles
+		assert_equal 9 ,@m.without_doubles(4,4,true)
+		assert_equal 5, @m.without_doubles(2,2,true)
+		assert_equal 8, @m.without_doubles(4,4,false)
+		assert_equal 7, @m.without_doubles(6,6,true)
+
+	end
 
 end
