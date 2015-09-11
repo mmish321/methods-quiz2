6a5689296d137524d75c50ee91633cbcfa6ee77a
Michelle Mao <maom@s.dcsdk12.org>
2015-09-11 09:53:12 -0600

    Tests for without_doubles

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,19 @@
-module MethodsQuiz
-	
-	
-
+module MethodsQuiz2
+	def without_doubles(roll1,roll2,no_doubles)
+		if no_doubles
+				if roll1 == roll2
+					if roll1 == 6 && roll2 ==6
+						roll1 + 1
+					else
+						roll1 = roll1 + 1
+						roll1 + roll2
+					end
+				else
+					roll1 + roll2
+				end
+		else
+			roll1 + roll2
+		end
+	end
 end
+
