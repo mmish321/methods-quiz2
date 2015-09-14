1a62943b8b944630275b84695b4a4d99c9c54758
Michelle Mao <maom@s.dcsdk12.org>
2015-09-14 10:18:46 -0600

    Refactored most of the code

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,70 +1,30 @@
 module MethodsQuiz2
 	def without_doubles(roll1,roll2,no_doubles)
-		if no_doubles
-				if roll1 == roll2
-					if roll1 == 6 && roll2 ==6
-						roll1 + 1
-					else
-						roll1 = roll1 + 1
-						roll1 + roll2
-					end
-				else
-					roll1 + roll2
-				end
-		else
-			roll1 + roll2
-		end
+		no_doubles ? (roll1 == roll2 ? increment(roll1, roll2) : roll1 + roll2) : roll1 + roll2
 	end
 	def max_maybe(int1,int2)
-		if int1 == int2
-			0
-		else
-			int1remain = int1%5
-			int2remain = int2%5
-			if int1remain == int2remain
-				if int1> int2
-					int2
-				else
-					int1
-				end
-			else
-				if int1 > int2
-					int1
-				else
-					int2
-				end
-			end
-		end		
-		
+		int1remain = int1%5
+		int2remain = int2%5
+		int = [int1, int2]
+		int1 == int2 ? 0 :(int1remain == int2remain ? int.min : int.max) 
 	end
+
 	def squirrels_play?(temp,summer)
-		if summer
-			if temp >= 60 && temp <= 100
-				true
-			else
-				false
-			end
-		else
-			if temp >= 60 && temp <= 90
-				true
-			else
-				false
-			end
-		end
-		
+		summer ? checking_temp_summer(temp) : checking_temp(temp)
 	end
 	def red_ticket(a,b,c)
-		if a ==b && b == c
-			if a ==2 && b == 2 && c == 2
-				10
-			else
-				5
-			end
-		elsif  b!= a && c != a
-			1
-		else
-			0
-		end
+		(a ==b && b == c) ? (a == 2 ? 10 : 5) : (b!= a && c != a ? 1 : 0)
+	end
+	private
+	def increment(roll1, roll2)
+		roll1 == 6 && roll2 == 6 ? 7 : (roll1+ 1) + roll2
+	end
+	def checking_temp(temp)
+		(temp >= 60 && temp <= 90) ? true : false
+	end
+	def checking_temp_summer(temp)
+		(temp >= 60 && temp <=100) ? true : false
 	end
 end
+
 
--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -4,39 +4,100 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative 'methods_quiz2'
 
-class MethodsQuizTest < MiniTest::Test
+class QuizTest < MiniTest::Test
+	i_suck_and_my_tests_are_order_dependent!
+
 	def setup
 		@m = Class.new do
      include MethodsQuiz2
    	end.new
 	end
+end
 
-	def test_without_doubles
-		assert_equal 9 ,@m.without_doubles(4,4,true)
-		assert_equal 5, @m.without_doubles(2,2,true)
-		assert_equal 8, @m.without_doubles(4,4,false)
-		assert_equal 7, @m.without_doubles(6,6,true)
-
+module MethodsQuiz2Tests
+	
+	class WithoutDoubles < QuizTest
+		def test_not_a_double
+			assert_equal 4, @m.without_doubles(1,3, false)
+			assert_equal 5, @m.without_doubles(3,2, true)
+		end
+		def test_double_without_no_doubles
+			assert_equal 8, @m.without_doubles(4, 4, false)
+			assert_equal 12, @m.without_doubles(6, 6, false)
+		end
+		def test_double_with_no_doubles
+			assert_equal 9, @m.without_doubles(4, 4, true)
+			assert_equal 3, @m.without_doubles(1, 1, true)
+		end
+		def test_wrap_around
+			assert_equal 7, @m.without_doubles(6, 6, true)
+		end
 	end
-	def test_max_maybe
-		assert_equal 0, @m.max_maybe(2,2)
-		assert_equal 6, @m.max_maybe(6,3)
-		assert_equal 10, @m.max_maybe(20,10)
-		
-	end
-	def test_squirrels_play
-		assert_equal true, @m.squirrels_play?(70, false)
-		assert_equal false, @m.squirrels_play?(20, false)
-		assert_equal false, @m.squirrels_play?(95, false)
-		assert_equal true, @m.squirrels_play?(98,true)
 
+	class MaxMaybe < QuizTest
+		def test_same_values
+			assert_equal 0, @m.max_maybe(5, 5)
+			assert_equal 0, @m.max_maybe(-6, -6)
+		end
+		def test_remainder_mod_5
+			assert_equal 5, @m.max_maybe(10, 5)
+			assert_equal 4, @m.max_maybe(4, 29)
+			assert_equal -10, @m.max_maybe(-5, -10)
+		end
+		def test_a_is_larger
+			assert_equal 10, @m.max_maybe(10, 4)
+		end
+		def test_b_is_larger
+			assert_equal 6, @m.max_maybe(2, 6)
+		end
+		def test_with_negatives
+			assert_equal -1, @m.max_maybe(-1, -4)
+			assert_equal -1, @m.max_maybe(-4, -1)
+		end
 	end
-	def test_red_ticket
-		assert_equal 10, @m.red_ticket(2,2,2)
-		assert_equal 5, @m.red_ticket(0,0,0)
-		assert_equal 1, @m.red_ticket(0,1,2)
-		assert_equal 0, @m.red_ticket(0,0,1)
 
+	class SquirrelsPlay < QuizTest
+		def test_just_right_temp
+			assert_equal true, @m.squirrels_play?(65, false)
+			assert_equal true, @m.squirrels_play?(65, true)
+		end
+		def test_low_temp
+			assert_equal false, @m.squirrels_play?(55, true)
+			assert_equal false, @m.squirrels_play?(55, false)
+		end
+		def test_limit
+			assert_equal true, @m.squirrels_play?(60, true)
+			assert_equal true, @m.squirrels_play?(60, false)
+			assert_equal false, @m.squirrels_play?(59, true)
+			assert_equal false, @m.squirrels_play?(59, false)
+			assert_equal false, @m.squirrels_play?(91, false)
+		end
+		def test_summer
+			assert_equal true, @m.squirrels_play?(91, true)
+			assert_equal false, @m.squirrels_play?(91, false)
+		end
+		def test_summer_limit
+			assert_equal true, @m.squirrels_play?(100, true)
+			assert_equal false, @m.squirrels_play?(101, true)
+		end
+	end
+	class RedTicket < QuizTest
+		def test_all_equals_two
+			assert_equal 10, @m.red_ticket(2,2,2)
+		end
+		def test_all_the_same
+			assert_equal 5 , @m.red_ticket(0,0,0)
+			assert_equal 5, @m.red_ticket(1,1,1)
+		end
+		def test_b_c_different
+			assert_equal 1, @m.red_ticket(0,1,2)
+			assert_equal 1, @m.red_ticket(1,0,2)
+			assert_equal 1, @m.red_ticket(2,1,0)
+		end
+		def test_b_c_same
+			assert_equal 0, @m.red_ticket(0,0,1)
+			assert_equal 0, @m.red_ticket(2,0,2)
+		end
 	end
 
 end
